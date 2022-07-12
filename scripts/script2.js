3c749cd2c13d89a0c0d81fe35974daa3eb31ddaa
Matheus Dutra <matheus_dutra_96@hotmail.com>
2022-07-12 18:39:23 -0300

    commit changes

--- scripts/script2.js
+++ scripts/script2.js
@@ -1,35 +1,3 @@
-let firstName = document.querySelector('input#first-name')
-let lastName = document.querySelector('input#last-name')
-let email = document.querySelector('input#email')
-let password = document.querySelector('input#password')
-
-firstName.addEventListener("keyup", checkFirstName)
-firstName.input = firstName.value
-
-lastName.addEventListener("keyup", checkLastName)
-lastName.input = lastName.value
-
-
-email.addEventListener("keyup", checkEmail)
-email.input = email.value
-
-password.addEventListener("keyup", checkPassword)
-password.input = password.value
-
-function checkFirstName(input) {
-
-    if (!input.currentTarget.value) {
-        errorMsg = document.querySelector('span#span-first-name')
-        errorMsg.innerHTML = 'First name cannot be empty'
-        errorMsg.style.display = 'block'
-
-    }
-    else {
-        errorMsg = document.querySelector('span#span-first-name')
-        errorMsg.style.display = 'none'
-    }
-}
-
 function invalidFirstName() {
     errorMsg = document.querySelector('span#span-first-name')
     errorMsg.innerHTML = 'First name cannot be empty'
@@ -40,22 +8,6 @@ function invalidFirstName() {
 function validFirstName() {
     errorMsg = document.querySelector('span#span-first-name')
     errorMsg.style.display = 'none'
-}
-
-function checkLastName(input) {
-
-    if (!input.currentTarget.value) {
-        errorMsg = document.querySelector('span#span-last-name')
-        errorMsg.innerHTML = 'Last name cannot be empty'
-        errorMsg.style.display = 'block'
-
-    }
-    else {
-        errorMsg = document.querySelector('span#span-last-name')
-        errorMsg.style.display = 'none'
-        count++
-
-    }
 }
 
 function invalidLastName() {
@@ -69,30 +21,6 @@ function validLastName() {
     errorMsg.style.display = 'none'
 }
 
-function checkEmail(input) {
-
-    if (!input.currentTarget.value) {
-        errorMsg = document.querySelector('span#span-email')
-        errorMsg.innerHTML = 'Email cannot be empty'
-        errorMsg.style.display = 'block'
-
-    }
-    else {
-        var re = /\S+@\S+\.\S+/
-
-        if (re.test(input.currentTarget.value)) {
-            errorMsg = document.querySelector('span#span-email')
-            errorMsg.style.display = 'none'
-
-        }
-        else {
-            errorMsg = document.querySelector('span#span-email')
-            errorMsg.innerHTML = 'Looks like this is not an email'
-            errorMsg.style.display = 'block'
-
-        }
-    }
-}
 
 function invalidEmail() {
     errorMsg = document.querySelector('span#span-email')
@@ -105,20 +33,6 @@ function validEmail() {
     errorMsg.style.display = 'none'
 }
 
-function checkPassword(input) {
-
-    if (!input.currentTarget.value) {
-        errorMsg = document.querySelector('span#span-password')
-        errorMsg.innerHTML = 'Password cannot be empty'
-        errorMsg.style.display = 'block'
-
-    }
-    else {
-        errorMsg = document.querySelector('span#span-password')
-        errorMsg.style.display = 'none'
-
-    }
-}
 
 function invalidPassword() {
     errorMsg = document.querySelector('span#span-password')
